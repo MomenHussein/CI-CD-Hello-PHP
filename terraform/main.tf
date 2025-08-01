9ce2817d361418a420fa32c5f4fc4bbc09eb56e8
Momen Hussein <98105343+MomenHussein@users.noreply.github.com>
2025-08-01 03:37:22 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,3 @@
-resource "aws_instance" "web_server" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "Terraform-Web-Server"
-  }
+module "kubernetes_resources" {
+  source = "./kubernetes"
 }
