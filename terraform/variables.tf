50c415944fa8ecfa437bdc6a6cf3b192d39650f1
Momen Hussein <98105343+MomenHussein@users.noreply.github.com>
2025-08-01 03:37:02 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,11 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
+variable "project_id" {
+  description = "GCP Project ID"
 }
 
-variable "aws_profile" {
-  description = "AWS CLI profile name"
-  type        = string
-  default     = "default"
+variable "region" {
+  default = "us-central1"
 }
 
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance"
-  type        = string
-  default     = "ami-0c02fb55956c7d316"  # Ubuntu 20.04 (us-east-1)
-}
-
-variable "instance_type" {
-  description = "Instance type for the EC2"
-  type        = string
-  default     = "t2.micro"
+variable "zone" {
+  default = "us-central1-c"
 }
