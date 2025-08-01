2361e4d48b715268ee1842a2e5d8a2e9dd1268e3
Momen Hussein <98105343+MomenHussein@users.noreply.github.com>
2025-08-01 03:37:38 +0300

    Delete terraform/outputs.tf

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.web_server.id
-}
-
-output "instance_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.web_server.public_ip
-}
