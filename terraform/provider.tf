048b803ece954b901529e552365fae7f1387a5f9
Momen Hussein <98105343+MomenHussein@users.noreply.github.com>
2025-08-01 03:36:36 +0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,24 @@
-provider "aws" {
-  region  = var.aws_region
-  profile = var.aws_profile
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+provider "kubernetes" {
+  host                   = google_container_cluster.primary.endpoint
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+}
+
+data "google_client_config" "default" {}
+
+resource "google_container_cluster" "primary" {
+  name     = "my-k8s-cluster"
+  location = var.zone
+
+  initial_node_count = 2
+
+  node_config {
+    machine_type = "e2-medium"
+  }
 }
